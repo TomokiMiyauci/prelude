50ff03d614691500f8d8fea21d266932f3d79f20
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2022-10-06 12:59:05 +0900

    revert: "feat(lists): improve type infer"
    This reverts commit 6a6db798346ef136f5ce6f713f6e2e86a5b6a459.

--- lists.ts
+++ lists.ts
@@ -13,7 +13,8 @@
 export function head<T extends readonly [unknown, ...readonly unknown[]]>(
   value: T,
 ): T[0];
-export function head<T>(value: Iterable<T>): T | undefined {
+export function head<T>(value: Iterable<T>): T | undefined;
+export function head(value: Iterable<unknown>): unknown {
   const [first] = value;
 
   return first;
