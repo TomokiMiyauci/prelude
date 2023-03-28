aee55ddf0ec3faab8f4413d92d9ff74742928c98
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-28 10:50:24 +0900

    refactor: remove unused

--- deps.ts
+++
@@ -1,2 +0,0 @@
-// deno-lint-ignore no-explicit-any
-export type AnyFn = (...args: any) => any;
--- dev_deps.ts
+++
@@ -1,8 +0,0 @@
-export * from "https://deno.land/std@0.158.0/testing/bdd.ts";
-export * from "https://deno.land/std@0.158.0/testing/asserts.ts";
-
-// deno-lint-ignore no-explicit-any
-export type Fn<F extends (...args: any) => any> = [
-  ...Parameters<F>,
-  ReturnType<F>,
-];
