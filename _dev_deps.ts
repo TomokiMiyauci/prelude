72002246356a4667e8b1a3a98c63c0b9feca37b1
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-28 10:52:40 +0900

    chore: update lock file

---
+++ _dev_deps.ts
@@ -0,0 +1,8 @@
+export {
+  assertSpyCallArgs,
+  assertSpyCalls,
+  spy,
+  stub,
+} from "https://deno.land/std@0.181.0/testing/mock.ts";
+export { assertEquals } from "https://deno.land/std@0.181.0/testing/asserts.ts";
+export { describe, it } from "https://deno.land/std@0.181.0/testing/bdd.ts";
